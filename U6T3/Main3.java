80969aa22a5d80183826cdb4c1c87b71ff57f9dd
Cherine Soewignjo <148895697+cherinesoe@users.noreply.github.com>
2025-01-02 14:10:31 +0000

    more arrays

---
+++ U6T3/Main3.java
@@ -0,0 +1,12 @@
+public class Main3 {
+    public static void main(String[] args) {
+
+        String[] strings9 = {"HELlo", "Halo", "WHAT!", "Adam", "what", "booyAH", "WHY?", "for"};
+        ArrayAlgorithms.makeUppercase(strings9);
+        for (String str : strings9) {
+            System.out.print(str + " ");
+        }
+        System.out.println();
+    
+    }
+}
